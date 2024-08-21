8a7a9de5c32b25d66a83d8b421e82f13d445d7d3
foroozan <foroozankashani0823@gmail.com>
2024-08-21 09:05:11 +0330

    register models - 4

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ChatRoom, Message 
 
-# Register your models here.
+@admin.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ('user1', 'user2', 'created_at')
+    search_fields = ('user1__username', 'user2__username')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'chatroom', 'date_sent', 'text_content')
+    list_filter = ('chatroom', 'sender')
+    search_fields = ('sender__username', 'text_content')
+    ordering = ('date_sent',)
+
